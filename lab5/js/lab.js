d6973c00fcc07e6ae9bfb3c3bcc27942efa86902
Tsukiiyakii <alphinathewolf22@gmail.com>
2025-04-23 17:59:53 -0700

    fix

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Alyssa Yuen
+// Date: 23 April
+// Lincense: Public Domain
 
-// Constants
 
-// Functions
+// Define Variables
+make = "BMW";
+model = "Mini Cooper";
+color = "Ocean Wave Green";
+year = 2025;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//calculate
+age = 2019 - year;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "years<br>");
