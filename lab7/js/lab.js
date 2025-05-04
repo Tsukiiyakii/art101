68a22fc5902a28898286eca8ae8fa19d3b5fe7f2
Tsukiiyakii <alphinathewolf22@gmail.com>
2025-05-04 05:54:10 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,28 @@
 // index.js - purpose and description here
 // Author: Alyssa Yuen
-// Date: 28 April
+// Date: 4 May
 // Lincense: Public Domain
 
 
-// Define Variables
-myTransport = ["BMW Mini Cooper","bus", "ride from friends"];
-myMainRide = {
-  make: "BMW",
-  model: "Mini Cooper",
-  color: "Ocean Wave Green",
-  year: 2025,
-  age: function () {
-      return 2025 - this.year;
-  }
+// sortUserName - a function
+function sortUserName() {
+  var userName = window.prompt("Hi. Let me fix your name");
+  console.log("userName =", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  // sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySortc=", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSOrted);
+  return nameSorted;
 }
+
+
+
+
 //output
-document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
-document.writeln("My Main Ride: <pre>",
-    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+document.writeln("Your name is now fixed: ",
+    sortUserName(), "</br");
+    
