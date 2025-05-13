591ae8bdb369392d78abfcde7d5e077fc589b85a
Tsukiiyakii <alphinathewolf22@gmail.com>
2025-05-12 23:12:17 -0700

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,28 +1,14 @@
-// index.js - purpose and description here
-// Author: Alyssa Yuen
-// Date: 4 May
-// Lincense: Public Domain
-
-
-// sortUserName - a function
-function sortUserName() {
-  var userName = window.prompt("Hi. Let me fix your name");
-  console.log("userName =", userName);
-  //split string to array
-  var nameArray = userName.split('');
-  // sort the array
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySortc=", nameArraySort);
-  //join array back to a string
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSOrted);
-  return nameSorted;
-}
-
-
-
-
-//output
-document.writeln("Your name is now fixed: ",
-    sortUserName(), "</br");
-    
+/*
+lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page
+Requirements: jQuery must be loaded for this script to work.
+Author: Alyssa Yuen
+Date: 12 May 2025
+Lincense: Public Domain
+*/
+// add buttom to challenge section
+$("#challenge").apend("<button id='button-challenge'>Make Special</button>");
+// add a click listner to the challenge button 
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+});
