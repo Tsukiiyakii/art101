8a45ef99ed8ee8d866bd3be9dbefbd2777d5c44b
Tsukiiyakii <alphinathewolf22@gmail.com>
2025-05-19 22:04:51 -0700

    11

---
+++ lab11/js/lab.js
@@ -0,0 +1,25 @@
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Alyssa Yuen
+   Date: 2025
+*/
+function generateRandomText() {
+  const text = "Meow, Me-OW, meow!!!, meow meow meow, meowwww, myeow, mew, nya, purr purr";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
+// click listener for button
+$("#make-convo").click(function(){
+// get new fake dialogue
+const newText = generateRandomText();
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
